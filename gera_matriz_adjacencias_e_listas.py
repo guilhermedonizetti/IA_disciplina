218a8b94ca6cea7172957b4df13fa355c392f244
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-08-20 08:50:42 -0300

    Matriz de adjacencias

---
+++ gera_matriz_adjacencias_e_listas.py
@@ -0,0 +1,22 @@
+from numpy import zeros
+from random import randint
+
+class EstruturasDados:
+
+    def __init__(self):
+        pass
+
+    def gerar_matriz(self):
+        qtd_cidades = int(input("Quantidade de cidades da rota: "))
+        self.matriz = zeros((qtd_cidades, qtd_cidades), dtype=int)
+        for i in self.matriz:
+            for g in self.matriz:
+                if i!=g:
+                    self.matriz[i][g] = randint(1, 10)
+        self.exibir_dados()
+    
+    def exibir_dados(self):
+        print(self.matriz)
+
+ed = EstruturasDados()
+ed.gerar_matriz()

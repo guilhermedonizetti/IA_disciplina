b12476de7dbbe7c0c26ffb7c9f0c78eaa4e86ea1
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-09-01 09:12:16 -0300

    Gera o caminho da Ajuda Hum. até uma cidade X e dela até um atendimento

--- agente.py
+++ agente.py
@@ -9,6 +9,9 @@ class Agente(Busca):
         self.pontos_ajuda_hum = ["GIORGIU", "FAGARAS",
                     "CRAIOVA", "ZERIND", "RIMNICU VILCEA"]
 
+        self.pontos_atendimento = ["PITESTI", "FAGARAS",
+                    "TIMISOARA", "ZERIND", "VASLUI"]
+
     def encontrar_caminho(self):
         """Metodo para encontrar um caminho entre dois pontos."""
         cidade_final = input("Ajuda Humanitária para (cidade): ")
@@ -21,6 +24,17 @@ class Agente(Busca):
                 rota = caminho
 
         print("\nA rota mais curta é: {}".format(rota))
+        self.encontrar_atendimento(cidade_final)
+
+    def encontrar_atendimento(self, cidade_inicial):
+        tam_caminho = 10000
+        for i in self.pontos_atendimento:
+            caminho = self.caminho(cidade_inicial.upper(), i)
+            if len(caminho) < tam_caminho:
+                tam_caminho = len(caminho)
+                rota = caminho
+
+        print("\nO caminho para oatendimento é: {}".format(rota))
 
 agente = Agente()
 agente.encontrar_caminho()

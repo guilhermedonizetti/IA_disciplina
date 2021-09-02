5d1159a675d89abdac599d8110daa131b8d7cddd
guilhermedonizetti <guilhermetecnologias@gmail.com>
2021-09-02 10:21:49 -0300

    Gera a rota para Ajuda Humanitaria com menos consumo de Memoria (metodo: Profundidade)

--- agente.py
+++ agente.py
@@ -2,9 +2,11 @@
 \naquele que chama os metodos conforme o necessario."""
 
 from main import Busca
+from time import time
 
 class Agente(Busca):
 
+    #Inicializa as listas que sao de valores padrao
     def __init__(self):
         self.pontos_ajuda_hum = ["CRUZEIRO", "GUARATINGUETÁ",
                     "SÃO SEBASTIÃO", "TAUBATÉ", "CAÇAPAVA"]
@@ -13,28 +15,55 @@ class Agente(Busca):
                     "UBATUBA", "QUELUZ", "TREMEMBÉ"]
 
     def encontrar_caminho(self):
-        """Metodo para encontrar um caminho entre dois pontos."""
-        cidade_final = input("Ajuda Humanitária para (cidade): ")
+        """Metodo para encontrar um caminho entre dois pontos.
+        Recebe a cidade que precisa da ajuda humanitaria e realiza as tentativas
+        de gerar uma rota partindo das cidades da lista 'pontos_ajuda_hum'."""
 
-        tam_caminho = 10000
-        for i in self.pontos_ajuda_hum:
+        cidade_final = input("Ajuda Humanitária para (cidade): ") #recebe a cidade que precisa de AH
+
+        tam_caminho = 100000
+        tempo_ini = time() #para verificar o desempenho
+        for i in self.pontos_ajuda_hum: #a cada cidade da lista, tenta um camino...
             caminho = self.caminho(i, cidade_final.upper())
+            #se o caminho for menor ele sera o atual
             if len(caminho) < tam_caminho:
                 tam_caminho = len(caminho)
                 rota = caminho
 
-        print("\nA rota mais curta é: {}".format(rota))
-        self.encontrar_atendimento(cidade_final)
+        print("\nA rota mais curta é: {}\nTempo: {}".format(rota, time()-tempo_ini))
+        self.encontrar_atendimento(cidade_final) #Gerar um caminho para o atndimento hospitalar
+        self.Profundidade(cidade_final) #Tenta outro metodo para gerar uma rota de ajuda huma.
 
     def encontrar_atendimento(self, cidade_inicial):
-        tam_caminho = 10000
-        for i in self.pontos_atendimento:
+        """Metodo para encontrar um caminho entre a cidade que recebeu a AH e o hospi mais proximo.
+        Recebe a cidade que teve a ajuda humanitaria e realiza as tentativas de gerar
+        uma rota partindo dessa cidade ate a mais proxima dentre as da lista 'pontos_atendimento'."""
+
+        tam_caminho = 100000
+        for i in self.pontos_atendimento: #para cada cidade da lista tenta uma rota...
             caminho = self.caminho(cidade_inicial.upper(), i)
+            #a menor rota sera a atual
             if len(caminho) < tam_caminho:
                 tam_caminho = len(caminho)
                 rota = caminho
 
         print("\nO caminho para o atendimento é: {}".format(rota))
+
+    def Profundidade(self, cidade_final):
+        """Metodo para encontrar um caminho entre dois pontos.
+        Recebe a cidade que precisa da ajuda humanitaria e realiza as tentativas
+        de gerar uma rota partindo das cidades da lista 'pontos_ajuda_hum' usando menos Memoria."""
+
+        tam_caminho = 100000
+        tempo_ini = time() #verifica o desempenho
+        for i in self.pontos_ajuda_hum: #para cada cidade da lista tenta um caminho...
+            caminho = self.caminho(i, cidade_final.upper())
+            #o menor caminho sera o atual
+            if len(caminho) < tam_caminho:
+                tam_caminho = len(caminho)
+                rota = caminho
+
+        print("\nProfundidade: {}\nTempo: {}".format(rota, time()-tempo_ini))
 
 agente = Agente()
 agente.encontrar_caminho()
--- main.py
+++ main.py
@@ -70,3 +70,69 @@ class Busca(Lista):
                         return caminho
 
         return "Caminho não encontrado."
+
+    #Metodo para Profundidade
+    def profundidade(self, inicio, fim):
+        
+        caminho = []
+
+        # manipular a FILA para a busca
+        l1 = Lista()
+
+        # cópia para apresentar o caminho (somente inserção)
+        l2 = Lista()
+
+        # insere ponto inicial como nó raiz da árvore
+        l1.insereUltimo(inicio,0,None)
+        l2.insereUltimo(inicio,0,None)
+
+        # controle de nós visitados
+        visitado = []
+        linha = []
+        linha.append(inicio)
+        linha.append(0)
+        visitado.append(linha)
+
+
+        while l1.vazio() == False:
+            # remove o primeiro da fila
+            atual = l1.deletaUltimo()
+            if atual is None: break
+
+            ind = nos.index(atual.valor1)
+
+            # varre todos as conexões dentro do grafo a partir de atual
+            for i in range(len(grafo[ind])-1,-1,-1):
+
+                novo = grafo[ind][i]
+                #print("\tFilho de atual: ",novo)
+                flag = True  # pressuponho que não foi visitado
+
+                # para cada conexão verifica se já foi visitado
+                for j in range(len(visitado)):
+                    if visitado[j][0]==novo:
+                        if visitado[j][1]<=(atual.valor2+1):
+                            flag = False
+                        else:
+                            visitado[j][1]=atual.valor2+1
+                        break
+                    
+                
+                # se não foi visitado inclui na fila
+                if flag:
+                    l1.insereUltimo(novo, atual.valor2 + 1, atual)
+                    l2.insereUltimo(novo, atual.valor2 + 1, atual)
+
+                    # marca como visitado
+                    linha = []
+                    linha.append(novo)
+                    linha.append(atual.valor2+1)
+                    visitado.append(linha)
+
+                    # verifica se é o objetivo
+                    if novo == fim:
+                        caminho += l2.exibeCaminho()
+                        #print("Árvore de busca:\n",l2.exibeLista())
+                        return caminho
+
+        return "caminho não encontrado"
